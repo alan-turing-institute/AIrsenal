5ff1395ddfd35baeda55aa3541f45a23132fc6b9
nbarlowATI <nbarlow@turing.ac.uk>
2018-08-05 22:27:48 +0100

    update framework and tests

---
+++ framework/schema.py
@@ -0,0 +1,75 @@
+"""
+Interface to the SQLite db.
+Use SQLAlchemy to convert between DB tables and python objects.
+"""
+
+from sqlalchemy import Column, ForeignKey, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+
+class Player(Base):
+    __tablename__ = "player"
+    player_id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    team = Column(String(100), nullable=False)
+    position = Column(String(100), nullable=False)
+    current_price = Column(Integer, nullable=True)
+    purchased_price = Column(Integer, nullable=True)
+#    scores = relationship("PlayerScore")
+#    fixtures = relationship("Fixture")
+
+class Match(Base):
+    __tablename__ = "match"
+    match_id = Column(Integer, primary_key=True, autoincrement=True)
+    date = Column(String(100), nullable=False)
+    season = Column(String(100), nullable=False)
+    gameweek = Column(Integer, nullable=True) # not there for 14/15 season
+    home_team = Column(String(100), nullable=False)
+    away_team = Column(String(100), nullable=False)
+    home_score = Column(Integer, nullable=False)
+    away_score = Column(Integer, nullable=False)
+
+
+class Fixture(Base):
+    __tablename__ = "fixture"
+    fixture_id = Column(Integer, primary_key=True, autoincrement=True)
+    date = Column(String(100), nullable=False)
+    gameweek = Column(Integer, nullable=False)
+    home_team = Column(String(100), nullable=False)
+    away_team = Column(String(100), nullable=False)
+
+
+class PlayerScore(Base):
+    __tablename__ = "player_score"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    player_id = Column(Integer, nullable=False)
+    match_id = Column(Integer, nullable=False)
+    player_team = Column(String(100), nullable=False)
+    opponent = Column(String(100), nullable=False)
+    points = Column(Integer, nullable=False)
+    goals = Column(Integer, nullable=False)
+    assists = Column(Integer, nullable=False)
+    bonus = Column(Integer, nullable=False)
+    conceded = Column(Integer, nullable=False)
+
+class PlayerPrediction(Base):
+    __tablename__ = "player_prediction"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    player_id = Column(Integer, nullable=False)
+    fixture_id = Column(Integer, nullable=False)
+    predicted_points = Column(Float, nullable=False)
+    method = Column(String(100), nullable=False)
+
+
+engine = create_engine('sqlite:////tmp/data.db')
+
+Base.metadata.create_all(engine)
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
