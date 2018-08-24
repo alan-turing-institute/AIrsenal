4203c71f116c6651fffbbd2969751c87c310cf5d
nbarlowATI <nbarlow@turing.ac.uk>
2018-08-24 22:48:42 +0800

    add Transactions table so we can keep track of our team

--- scripts/fill_db.sh
+++ scripts/fill_db.sh
@@ -2,5 +2,6 @@ python fill_player_table.py
 python fill_match_table.py
 python fill_fixture_table.py
 python fill_playerscore_table.py
+python fill_playerscore_this_season.py
 python fill_predictedscore_table.py "EP"
-
+python fill_transaction_table.py
