99a0ea26e7f6905f409383694fa192c9bffa1853
anguswilliams91 <anguswilliams91@gmail.com>
2018-09-27 12:07:14 +0100

    try something else:

--- setup.py
+++ setup.py
@@ -1,36 +1,8 @@
-from setuptools import setup, find_packages
-from setuptools.command.install import install
-from distutils.command.install import install as _install
-
-import os
-import subprocess
-import sys
+from setuptools import setup
 
 with open("requirements.txt", "r") as f:
     REQUIRED_PACKAGES = f.read().splitlines()
-SETUP_DIR = os.path.dirname(os.path.abspath(__file__))
-
-
-class install_(install):
-    # inject your own code into this func as you see fit
-
-    def run(self):
-
-        ret = None
-        if self.old_and_unmanageable or self.single_version_externally_managed:
-            ret = _install.run(self)
-        else:
-            caller = sys._getframe(2)
-            caller_module = caller.f_globals.get('__name__','')
-            caller_name = caller.f_code.co_name
-
-            if caller_module != 'distutils.dist' or caller_name!='run_commands':
-                _install.run(self)
-            else:
-                self.do_egg_install()
-        sub_path = "{}/bpl".format(SETUP_DIR)
-        subprocess.check_call([sys.executable, "-m", "pip", "install", sub_path])
-        return ret
+REQUIRED_PACKAGES.append("bpl")
 
 
 setup(
@@ -43,6 +15,6 @@ setup(
     packages=["airsenal"],
     install_requires=REQUIRED_PACKAGES,
     setup_requires=REQUIRED_PACKAGES,
-    cmdclass={"install": install_}
+    dependency_links=["git+ssh://git@github.com:anguswilliams91/bpl.git"]
 )
 
