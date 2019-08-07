22c837ece2d50799f76614d2c3c49071ed11bd61
nbarlowATI <nbarlow@turing.ac.uk>
2019-08-07 08:57:44 +0100

    update make_results_csv.py script to use Football Data API

--- airsenal/scripts/make_results_csv.py
+++ airsenal/scripts/make_results_csv.py
@@ -1,36 +1,20 @@
-#!/usr/bin/env python
-
 """
-quick'n'dirty python script to convert results copy/pasted from
-http://www.skysports.com/premier-league-results/YYYY-YY
-
-Usage example:
-python make_results_csv.py 2017
-(to get results for 2017-18 season
+Use the football data API to get past years data
+Usage:
+python make_results_from_fd.py 2018
 """
-
 import sys
-import os
-import re
-from datetime import datetime
-
-
-from ..framework.utils import get_gameweek_by_date
-
-date_regex = re.compile("day ([\d]+)[a-z]{2}[\s]+([\w]+)")
-score_regex = re.compile("([\d])[\s]+([\d])[\s]+([\w\s]+[\w])[\s]+FT")
+from airsenal.framework.data_fetcher import *
+from airsenal.framework.mappings import alternative_team_names
 
 start_year = sys.argv[-1]
 start_year_short = start_year[-2:]
 end_year_short = str(int(start_year_short) + 1)
 end_year = "20" + end_year_short
 
-infilename = "../data/results{}{}.txt".format(start_year_short, end_year_short)
 outfilename = "../data/results_{}{}_with_gw.csv".format(
     start_year_short, end_year_short
 )
-
-infile = open(infilename)
 outfile = open(outfilename, "w")
 outfile.write("date,home_team,away_team,home_score,away_score,gameweek\n")
 
@@ -39,28 +23,21 @@ away_team = ""
 datestr = ""
 
 gameweek = 0
+md = MatchDataFetcher()
 
-for line in infile.readlines():
-    if date_regex.search(line):
-        day, month = date_regex.search(line).groups()
-        if month in ["January", "February", "March", "April", "May"]:
-            year = end_year
-        else:
-            year = start_year
-        date = datetime.strptime("{} {} {}".format(day, month[:3], year), "%d %b %Y")
-        datestr = str(date)
-        print("Found date {}".format(date))
-        if start_year == "2018":
-            gameweek = get_gameweek_by_date(datestr)
-        last_line_was_score = False
-    elif score_regex.search(line):
-        home_score, away_score, away_team = score_regex.search(line).groups()
-        outfile.write(
-            "{},{},{},{},{},{}\n".format(
-                datestr, home_team, away_team, home_score, away_score, gameweek
-            )
-        )
-    else:
-        home_team = line.strip()
-
+for gw in range(1,39):
+    results = md.get_results(gw, start_year)
+    for result in results:
+        date = result[0].split("T")[0]
+        home_team = alternative_team_names[result[1]][1]
+        away_team = alternative_team_names[result[2]][1]
+        home_score = result[3]
+        away_score = result[4]
+        outfile.write("{},{},{},{},{},{}\n".format(date,
+                                                   home_team,
+                                                   away_team,
+                                                   home_score,
+                                                   away_score,
+                                                   gw))
+        print("{} {} {} {} {} {}".format(gw, date, home_team, away_team, home_score, away_score))
 outfile.close()
