85e1375416f8d5b494957a96b737ab2a0dde04d5
nbarlowATI <nbarlow@turing.ac.uk>
2019-08-08 08:57:27 +0100

    Use fifa ratings table again in BPL team model

--- airsenal/framework/bpl_interface.py
+++ airsenal/framework/bpl_interface.py
@@ -128,9 +128,8 @@ def get_team_model(df, df_X):
     Get the team-level stan model, which can give probabilities of
     each potential scoreline in a given fixture.
     """
-    ## TEMP - GET NAN ERROR IF USE DF_X
-#    model_team = bpl.BPLModel(df, X=df_X)
-    model_team = bpl.BPLModel(df, X=None)
+    model_team = bpl.BPLModel(df, X=df_X)
+#    model_team = bpl.BPLModel(df, X=None)
     model_team.fit()
     return model_team
 
