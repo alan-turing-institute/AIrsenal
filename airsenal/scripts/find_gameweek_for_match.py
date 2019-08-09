9e4e5c56e51bb415f83e8fc4d17a9553eb001f8e
Jack Roberts <jroberts@turing.ac.uk>
2019-08-09 15:07:52 +0100

    first attempt at fix

--- airsenal/scripts/find_gameweek_for_match.py
+++ airsenal/scripts/find_gameweek_for_match.py
@@ -2,89 +2,60 @@
 
 import sys
 import json
+import pandas as pd
 
 
 """
-The results_xxyy.csv don't have gameweek info in, but we should be able to
-match them up with the detailed player info in player_details_xxyy.json
+The results_xxyy.csv don't have gameweek info in, create them by looking
+at gameweek dates in FPL_xxyy.json file.
+
+!!! WARNING !!! FPL_xxyy.json file must be the latest available version as
+deadlines can change during the season. Using an out of date version can
+therefore lead to matches being assigned to wrong gameweek. 
+It may also be more robust to fix previous version of this file using player
+score files, rather than relying on the approach used here.
 """
 
-team_name_json = json.load(open("../data/alternative_team_names.json"))
+
+def get_gameweek_deadlines(fpl_file_path):
+    with open(fpl_file_path, 'r') as f:
+        fpl_json = json.load(f)
+
+    deadlines = pd.Series({e["id"]: e["deadline_time"]
+                          for e in fpl_json["events"]})
+    
+    deadlines = pd.to_datetime(deadlines).dt.date
+    deadlines.sort_index(inplace=True)
+    
+    return deadlines
 
 
-def find_players_for_team(team, summary_data):
-    """
-    return a list of player names who played for specified team
-    in the summary data
-    """
-    # map given team name to the name used in the summary data json
-    players = []
-    #    print("Looking for players from {}".format(home_team))
-    for player in summary_data:
-        for k, v in team_name_json.items():
-            if player["team"] in v and (team == k or team in v):
-                #                print("{} played for {}".format(player["name"], k))
-                players.append(player["name"])
-    return players
-
-
-def find_matches_against_team(team, player_list, detail_data):
-    """
-    loop through player list in the detail_data,
-    and find any gameweeks where the
-    opponent matches the specified team.
-    """
-    gameweeks = []
-    for player in player_list:
-        player_detail = detail_data[player]
-        for match in player_detail:
-
-            #        for k,v in player_detail.items():
-            for tk, tv in team_name_json.items():
-                if match["opponent"] in tv and (team == tk or team in tv):
-                    gameweeks.append(int(match["gameweek"]))
-        if len(gameweeks) == 2:
-            break
-    gameweeks.sort()
-    return gameweeks
+def get_gameweek_from_date(date, deadlines):
+    date = pd.to_datetime(date, dayfirst=True).date()
+    
+    gw = deadlines[date >= deadlines]
+    print("GW{} (deadline {})".format(gw.index[-1], gw.values[-1]))
+    return gw.index[-1]
 
 
 if __name__ == "__main__":
-
     season = sys.argv[-1]
+    
     results_file = open("../data/results_{}.csv".format(season))
     output_file = open("../data/results_{}_with_gw.csv".format(season), "w")
-    summary_data = json.load(open("../data/player_summary_{}.json".format(season)))
-    detail_data = json.load(open("../data/player_details_{}.json".format(season)))
-
-    seen_matches = []
-    linecount = 0
-    for line in results_file.readlines():
+    fpl_file_path = "../data/FPL_{}.json".format(season)
+    
+    deadlines = get_gameweek_deadlines(fpl_file_path)
+    
+    for linecount, line in enumerate(results_file.readlines()):
         if linecount == 0:
             output_file.write(line.strip() + ",gameweek\n")
-            linecount += 1
             continue
+        
         date, home_team, away_team = line.split(",")[:3]
         print(date, home_team, away_team)
-        home_players = find_players_for_team(home_team, summary_data)
-        gameweeks = find_matches_against_team(away_team, home_players, detail_data)
-        ## remember the results csv is in reverse time order,
-        ## so if we haven't already seen the reverse fixture,
-        ## we must be in the second of the two possible gameweeks.
-        gameweek = -1
-        if len(gameweeks) == 2:
-            if (away_team, home_team) in seen_matches:
-                print("Already saw {} vs {}".format(away_team, home_team))
-                gameweek = gameweeks[0]
-            else:
-                gameweek = gameweeks[1]
-        else:
-            print(
-                "Some sort of problem, only saw {} gameweeks for {} vs {}".format(
-                    len(gameweeks), home_team, away_team
-                )
-            )
-        seen_matches.append((home_team, away_team))
+        gameweek = get_gameweek_from_date(date, deadlines)
         output_file.write(line.strip() + "," + str(gameweek) + "\n")
 
+    results_file.close()
     output_file.close()
