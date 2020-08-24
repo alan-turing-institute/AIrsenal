73f1f92385f7cfaf6b772fadfa41e610c02b8007
Jack Roberts <jroberts@turing.ac.uk>
2020-08-24 11:01:34 +0100

    run black

--- airsenal/scripts/airsenal_run_pipeline.py
+++ airsenal/scripts/airsenal_run_pipeline.py
@@ -1,14 +1,29 @@
 import sys
 import os
-import click
 import multiprocessing
+
+import click
+
 from airsenal import TMPDIR
 
+
 @click.command("airsenal_run_pipeline")
-@click.option('--num_thread', type=int, default=None, help='No. of threads to use for pipeline run')
-@click.option('--weeks_ahead', type=int, default=3, help='No of weeks to use for pipeline run')
-@click.option('--bank', type=int, default=0, help='Amount in Bank for pipeline run')
-@click.option('--num_free_transfers', type=int, default=1, help='Number of free transfer for pipeline run')
+@click.option(
+    "--num_thread",
+    type=int,
+    default=None,
+    help="No. of threads to use for pipeline run",
+)
+@click.option(
+    "--weeks_ahead", type=int, default=3, help="No of weeks to use for pipeline run"
+)
+@click.option("--bank", type=int, default=0, help="Amount in Bank for pipeline run")
+@click.option(
+    "--num_free_transfers",
+    type=int,
+    default=1,
+    help="Number of free transfer for pipeline run",
+)
 def airsenal_run_pipeline(num_thread, weeks_ahead, bank, num_free_transfers):
     if not num_thread:
         num_thread = multiprocessing.cpu_count()
@@ -45,21 +60,23 @@ def setup_database():
     """
     Set up database
     """
-    os.system('airsenal_setup_initial_db')
+    os.system("airsenal_setup_initial_db")
 
 
 def update_database():
     """
     Update database
     """
-    os.system('airsenal_update_db --noattr')
+    os.system("airsenal_update_db --noattr")
 
 
 def run_prediction(num_thread, weeks_ahead):
     """
     Run prediction
     """
-    cmd = "airsenal_run_prediction --num_thread {} --weeks_ahead {}".format(num_thread, weeks_ahead)
+    cmd = "airsenal_run_prediction --num_thread {} --weeks_ahead {}".format(
+        num_thread, weeks_ahead
+    )
     os.system(cmd)
 
 
@@ -67,8 +84,9 @@ def run_optimization(num_thread, weeks_ahead, bank, num_free_transfers):
     """
     Run optimization
     """
-    cmd = "airsenal_run_optimization --num_thread {} --weeks_ahead {} --bank {} --num_free_transfers {}".\
-        format(num_thread, weeks_ahead, bank, num_free_transfers)
+    cmd = "airsenal_run_optimization --num_thread {} --weeks_ahead {} --bank {} --num_free_transfers {}".format(
+        num_thread, weeks_ahead, bank, num_free_transfers
+    )
     os.system(cmd)
 
 
