2c0cd7c660135d474ff1ec0fba85dcba14628873
Jack Roberts <jroberts@turing.ac.uk>
2020-10-20 23:30:22 +0100

    remove conflicting CURRENT_TEAMS and fixture_probabilities

--- airsenal/framework/season.py
+++ airsenal/framework/season.py
@@ -4,7 +4,7 @@ Season details
 
 """
 from datetime import datetime
-from airsenal.framework.data_fetcher import FPLDataFetcher
+
 
 def get_current_season():
     """
@@ -21,8 +21,3 @@ def get_current_season():
 
 # make this a global variable in this module, import into other modules
 CURRENT_SEASON = get_current_season()
-
-
-fetcher = FPLDataFetcher()
-# TODO make this a database table so we can look at past seasons
-CURRENT_TEAMS = [t["short_name"] for t in fetcher.get_current_team_data().values()]
