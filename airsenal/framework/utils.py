alan-turing-institute/AIrsenal

commit a2820c8c1fa5d330ec2a51b4f83fa79e431228f4

fix list_players returning empty list if database not current
return most recently available players rather than an empty list

airsenal/framework/utils.py
@@ -438,9 +438,17 @@ def list_players(
     if not dbsession:
         dbsession = session
 
-    # if trying to get players from the future, return current players
-    if season == CURRENT_SEASON and gameweek > NEXT_GAMEWEEK:
-        gameweek = NEXT_GAMEWEEK
+    # if trying to get players from after DB has filled, return most recent players
+    if season == CURRENT_SEASON:
+        last_gw = get_last_complete_gameweek_in_db(
+            season=CURRENT_SEASON, dbsession=dbsession
+        )
+        if gameweek > last_gw:
+            print(
+                f"WARNING: Incomplete data in DB for GW{gameweek}, "
+                f"returning players from GW{last_gw}."
+            )
+            gameweek = last_gw
 
     gameweeks = [gameweek]
     # check if the team (or all teams) play in the specified gameweek, if not

commit a50b292762215b896c02db7717d722a890bb566a

fix None comparison error in list_players if no complete gameweek in db
caused by a2820c8c1fa5d330ec2a51b4f83fa79e431228f4

airsenal/framework/utils.py
@@ -442,7 +442,13 @@ def list_players(
         last_gw = get_last_complete_gameweek_in_db(
             season=CURRENT_SEASON, dbsession=dbsession
         )
-        if gameweek > last_gw:
+        if last_gw is None and gameweek != 1:
+            print(
+                f"WARNING: No complete gameweek in DB for {season} season, "
+                f"returning players from GW1."
+            )
+            gameweek = 1
+        elif last_gw is not None and gameweek > last_gw:
             print(
                 f"WARNING: Incomplete data in DB for GW{gameweek}, "
                 f"returning players from GW{last_gw}."