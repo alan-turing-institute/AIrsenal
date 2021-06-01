e2aa0b1b1f8a2baab2007a9549ac34e71a4e99c0
jack89roberts <jroberts@turing.ac.uk>
2021-06-01 11:39:11 +0100

    add airsenal transfer and points history for 20/21 season

--- airsenal/scripts/dump_api.py
+++ airsenal/scripts/dump_api.py
@@ -10,3 +10,15 @@ with open(f"../data/FPL_{CURRENT_SEASON}.json", "w") as f:
 fixtures = fetcher.get_fixture_data()
 with open(f"../data/fixture_data_{CURRENT_SEASON}.json", "w") as f:
     json.dump(fixtures, f)
+
+history = fetcher.get_fpl_team_history_data()
+with open(f"../data/airsenal_history_{CURRENT_SEASON}.json", "w") as f:
+    json.dump(history, f)
+
+transfers = fetcher.get_fpl_transfer_data()
+with open(f"../data/airsenal_transfer_{CURRENT_SEASON}.json", "w") as f:
+    json.dump(transfers, f)
+
+gws = [fetcher.get_fpl_team_data(gw) for gw in range(1, 39)]
+with open(f"../data/airsenal_gw_{CURRENT_SEASON}.json", "w") as f:
+    json.dump(gws, f)
