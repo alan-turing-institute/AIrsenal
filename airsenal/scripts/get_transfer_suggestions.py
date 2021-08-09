5d9a7d4a25b12ebf2e293d27feb0017a3ad190e8
nbarlowATI <nbarlow@turing.ac.uk>
2021-08-09 13:32:19 +0100

    remove unnecesary argument for get_transfer_suggestions

--- airsenal/scripts/get_transfer_suggestions.py
+++ airsenal/scripts/get_transfer_suggestions.py
@@ -9,14 +9,14 @@ from airsenal.framework.schema import TransferSuggestion
 from airsenal.framework.utils import session, get_player_name
 
 
-def get_transfer_suggestions(currentsession, Suggestions):
+def get_transfer_suggestions(dbsession):
     """
     query the transfer_suggestion table.  Each row of the table
     will be in individual player in-or-out in a gameweek - we
     therefore need to group together all the rows that correspond
     to the same transfer strategy.  We do this using the "timestamp".
     """
-    all_rows = currentsession.query(Suggestions).all()
+    all_rows = dbsession.query(TransferSuggestion).all()
     last_timestamp = all_rows[-1].timestamp
     return (
         session.query(TransferSuggestion)
