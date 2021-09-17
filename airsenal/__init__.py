f602d40a7db3ad0b9dc32fbcd7dffe04c346a843
jack89roberts <jroberts@turing.ac.uk>
2021-09-17 16:16:29 +0100

    bump version to v1.1.0

--- airsenal/__init__.py
+++ airsenal/__init__.py
@@ -6,7 +6,7 @@ import os
 import tempfile
 
 # AIrsenal package version.
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 # Cross-platform temporary directory
 TMPDIR = "/tmp/" if os.name == "posix" else tempfile.gettempdir()
