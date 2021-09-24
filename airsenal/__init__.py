f59be352635a8ea1549a4fa414debf9937402591
jack89roberts <jroberts@turing.ac.uk>
2021-09-24 15:09:10 +0100

    release airsenal 1.1.1

--- airsenal/__init__.py
+++ airsenal/__init__.py
@@ -6,7 +6,7 @@ import os
 import tempfile
 
 # AIrsenal package version.
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 # Cross-platform temporary directory
 TMPDIR = "/tmp/" if os.name == "posix" else tempfile.gettempdir()
