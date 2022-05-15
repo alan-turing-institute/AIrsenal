7c87ffa420c335fb336f64ac7a8989068e0112c2
jack89roberts <jroberts@turing.ac.uk>
2022-05-15 10:55:09 +0100

    add entry point for saving data from api script

--- airsenal/scripts/dump_api.py
+++ airsenal/scripts/dump_api.py
@@ -3,22 +3,29 @@ import json
 from airsenal.framework.season import CURRENT_SEASON
 from airsenal.framework.utils import fetcher
 
-sdata = fetcher.get_current_summary_data()
-with open(f"../data/FPL_{CURRENT_SEASON}.json", "w") as f:
-    json.dump(sdata, f)
 
-fixtures = fetcher.get_fixture_data()
-with open(f"../data/fixture_data_{CURRENT_SEASON}.json", "w") as f:
-    json.dump(fixtures, f)
+def main():
+    """Save all data from the FPL API, e.g. at the end of the season."""
+    sdata = fetcher.get_current_summary_data()
+    with open(f"../data/FPL_{CURRENT_SEASON}.json", "w") as f:
+        json.dump(sdata, f)
 
-history = fetcher.get_fpl_team_history_data()
-with open(f"../data/airsenal_history_{CURRENT_SEASON}.json", "w") as f:
-    json.dump(history, f)
+    fixtures = fetcher.get_fixture_data()
+    with open(f"../data/fixture_data_{CURRENT_SEASON}.json", "w") as f:
+        json.dump(fixtures, f)
 
-transfers = fetcher.get_fpl_transfer_data()
-with open(f"../data/airsenal_transfer_{CURRENT_SEASON}.json", "w") as f:
-    json.dump(transfers, f)
+    history = fetcher.get_fpl_team_history_data()
+    with open(f"../data/airsenal_history_{CURRENT_SEASON}.json", "w") as f:
+        json.dump(history, f)
 
-gws = [fetcher.get_fpl_team_data(gw) for gw in range(1, 39)]
-with open(f"../data/airsenal_gw_{CURRENT_SEASON}.json", "w") as f:
-    json.dump(gws, f)
+    transfers = fetcher.get_fpl_transfer_data()
+    with open(f"../data/airsenal_transfer_{CURRENT_SEASON}.json", "w") as f:
+        json.dump(transfers, f)
+
+    gws = [fetcher.get_fpl_team_data(gw) for gw in range(1, 39)]
+    with open(f"../data/airsenal_gw_{CURRENT_SEASON}.json", "w") as f:
+        json.dump(gws, f)
+
+
+if __name__ == "__main__":
+    main()
