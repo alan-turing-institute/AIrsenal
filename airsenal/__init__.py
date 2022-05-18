6b848581422b0cf1fe2ee74fb5fc53cbaba59644
jack89roberts <jroberts@turing.ac.uk>
2022-05-18 22:23:28 +0100

    get airsenal version from package metadata

--- airsenal/__init__.py
+++ airsenal/__init__.py
@@ -5,8 +5,10 @@ ___init__.py for airsenal
 import os
 import tempfile
 
+import pkg_resources
+
 # AIrsenal package version.
-__version__ = "1.3.3"
+__version__ = pkg_resources.get_distribution("airsenal").version
 
 # Cross-platform temporary directory
 TMPDIR = "/tmp/" if os.name == "posix" else tempfile.gettempdir()
