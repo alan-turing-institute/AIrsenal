1c9fd3d7a19f73869b3d3dce41f1808c46b768aa
Luke Hare <lhare@turing.ac.uk>
2023-06-15 16:32:47 +0100

    Add num_samples attribute to random model

--- airsenal/framework/random_team_model.py
+++ airsenal/framework/random_team_model.py
@@ -6,11 +6,12 @@ import jax.numpy as jnp
 class RandomMatchPredictor(BaseMatchPredictor):
     """A Random model for predicting match outcomes."""
 
-    def __init__(self):
+    def __init__(self, num_samples=1000):
         self.teams = None
         self.attack = None
         self.defence = None
         self.home_advantage = None
+        self.num_samples = num_samples
     
     def fit(self, training_data: Dict[str, Union[Iterable[str], Iterable[float]]],
         random_state: int = 42):
@@ -21,11 +22,11 @@ class RandomMatchPredictor(BaseMatchPredictor):
         home_ind = jnp.array([self.teams.index(t) for t in home_team])
         away_ind = jnp.array([self.teams.index(t) for t in away_team])
         
-        self.attack = np.random.randn(1000, len(self.teams))
-        self.defence = np.random.randn(1000, len(self.teams))
-        self.home_advantage = np.random.randn(1000, len(self.teams))
-        self.corr_coef = np.random.randn(1000, )
-        self.rho = np.random.randn(1000, )
+        self.attack = np.random.randn(self.num_samples, len(self.teams))
+        self.defence = np.random.randn(self.num_samples, len(self.teams))
+        self.home_advantage = np.random.randn(self.num_samples, len(self.teams))
+        self.corr_coef = np.random.randn(self.num_samples, )
+        self.rho = np.random.randn(self.num_samples, )
         return self
 
     def predict_score_proba(self,
@@ -38,19 +39,19 @@ class RandomMatchPredictor(BaseMatchPredictor):
         home_team = [home_team] if isinstance(home_team, str) else home_team
         away_team = [away_team] if isinstance(away_team, str) else away_team
 
-        home_probs = np.random.randn(1000, len(home_team))
-        away_probs = np.random.randn(1000, len(away_team))
+        home_probs = np.random.randn(self.num_samples, len(home_team))
+        away_probs = np.random.randn(self.num_samples, len(away_team))
 
-        sampled_probs = np.random.randn(1000, len(home_team)) * home_probs * away_probs
+        sampled_probs = np.random.randn(self.num_samples, len(home_team)) * home_probs * away_probs
         return sampled_probs.mean(axis=0)
 
     def add_new_team(self, team_name: str, team_covariates: Optional[np.array] = None):
         if team_name in self.teams:
             raise ValueError("Team {} already known to model.".format(team_name))
 
-        attack = np.random.randn(1000,)
-        defence = np.random.randn(1000,)
-        home_advantage = np.random.randn(1000,)
+        attack = np.random.randn(self.num_samples,)
+        defence = np.random.randn(self.num_samples,)
+        home_advantage = np.random.randn(self.num_samples,)
 
         self.teams.append(team_name)
         self.attack = jnp.concatenate((self.attack, attack[:, None]), axis=1)
